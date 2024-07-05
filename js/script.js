bdcfbcc292aee42c1fec325ac1271ef40c059594
Nahid Hasan <nahid.h.kiran@gmail.com>
2024-07-05 08:21:11 +0600

    Header section added

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-icon").addEventListener("click", () => {
+    const mobileMenuContent = document.getElementById("mobile-menu-content");
+    mobileMenuContent.classList.toggle("display-mobile-menu")
+})
